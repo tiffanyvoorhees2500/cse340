4c6cdcc0efd87dfa3fe622baa0c2ae8ee179447d
tiffster2500 <tiff6377@hotmail.com>
2024-05-10 13:21:37 -0600

    change css on classification grid pictures

--- utilities/index.js
+++ utilities/index.js
@@ -35,21 +35,21 @@ Util.buildClassificationGrid = async function(data){
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => { 
       grid += '<li>'
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors"></a>'
-      grid += '<div class="namePrice">'
-      grid += '<hr>'
-      grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
-      grid += '</h2>'
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
-      grid += '</div>'
+        grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+          + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+          + 'details"><img src="' + vehicle.inv_thumbnail 
+          +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+          +' on CSE Motors"></a>'
+        grid += '<div class="namePrice">'
+          grid += '<hr>'
+          grid += '<h2>'
+            grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+              + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+              + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+          grid += '</h2>'
+          grid += '<span>$' 
+            + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+        grid += '</div>'
       grid += '</li>'
     })
     grid += '</ul>'
