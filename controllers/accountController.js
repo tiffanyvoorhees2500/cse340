f8294bec5bcba5f42748d62baf80d52c866b81e4
tiffster2500 <tiff6377@hotmail.com>
2024-05-15 15:38:28 -0600

    Final team activity adjustment

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,8 +1,9 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 /* ****************************************
-*  Deliver login view
+*  Deliver Login view
 * *************************************** */
 async function buildLogin(req, res, next){
     let nav = await utilities.getNav()
@@ -13,6 +14,9 @@ async function buildLogin(req, res, next){
     })
 }
 
+/* ****************************************
+*  Deliver Registration view
+* *************************************** */
 async function buildRegister(req, res, next){
     let nav = await utilities.getNav()
     res.render("account/register",{
@@ -29,11 +33,25 @@ async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -44,6 +62,7 @@ async function registerAccount(req, res) {
     res.status(201).render("account/login", {
       title: "Login",
       nav,
+      errors: null,
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
