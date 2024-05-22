09fb3fa9090e71eda97d07f9fe93ad930dfc1a92
tiffster2500 <tiff6377@hotmail.com>
2024-05-22 17:46:10 -0600

    reformatted code in inventoryRoute

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,28 +6,30 @@ const utilities = require("../utilities/")
 const invValidate= require("../utilities/inventory-validation")
 
 
-// Manually entered route to build inventory management view
+// Default inventory route
 router.get("/", utilities.handleErrors(invController.buildInventoryManager));
+
+/* ----------------- Dealing with classifications ------------------------ */
+// Route to build inventory by classification view
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 // Route to build add-classification view
 router.get("/add-classification",utilities.handleErrors(invController.buildAddClassification));
-router.post(
-    "/add-classification",
+router.post("/add-classification",
     invValidate.classificationRules(),
     invValidate.checkClassificationData,
     utilities.handleErrors(invController.addClassification)
 );
+
+/* ----------------- Dealing with classifications ------------------------ */
+// Route to build inventory by inventory detail view
+router.get("/detail/:inventoryId",utilities.handleErrors(invController.buildByInventoryId));
 // Route to build add-inventory view
 router.get("/add-inventory",utilities.handleErrors(invController.buildAddInventory));
-router.post(
-    "/add-inventory",
+router.post("/add-inventory",
     invValidate.inventoryRules(),
     invValidate.checkInventoryData,
     utilities.handleErrors(invController.addInventory)
 );
-// Route to build inventory by classification view
-router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
-// Route to build inventory by inventory detail view
-router.get("/detail/:inventoryId",utilities.handleErrors(invController.buildByInventoryId));
 router.get("/getInventory/:classification_id",utilities.handleErrors(invController.getInventoryJSON));
 
 module.exports = router;
